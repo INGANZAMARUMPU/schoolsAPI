4cfb53a3cde749a9f42da87973e0941dc54d8b02
konstrictor <jonkur96@gmail.com>
2020-10-13 23:24:41 +0200

    initial commit

---
+++ schoolsAPI/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for schoolsAPI project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schoolsAPI.settings')
+
+application = get_wsgi_application()
